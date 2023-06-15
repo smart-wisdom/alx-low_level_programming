c4f015706f796aa51bf09628219f288f8885cedd
smart wisdom <smartwisdom212@gmail.com>
2023-06-15 15:55:54 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a new line using the printf function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art, \n")
+		return (0);
+}
