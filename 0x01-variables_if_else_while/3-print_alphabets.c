db09bc35eeb72d087bbffe18ef0575bab91eac04
smart wisdom <smartwisdom212@gmail.com>
2023-06-17 09:55:37 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,27 +5,24 @@
  */
 int main(void)
 {
-
 	char a;
 
 	char b;
 
 	a = 'a';
 	b = 'A';
-
 	while
 		(a <= 'z');
 	{
-		putchar(a);
-		a++;
-}
-
+			putchar(a);
+			a++;
+		}
 	while
 		(b <= 'Z');
 	{
-		putchar(b);
-		b++;
-}
+			putchar(b);
+			b++;
+		}
 	putchar('\n');
 	return (0);
 }
