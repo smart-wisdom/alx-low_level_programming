fbcbd3b86076d04974010f8e10a04f16acdfa1a8
smart wisdom <smartwisdom212@gmail.com>
2023-06-17 19:40:30 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,14 +5,14 @@
  */
 int main(void)
 {
-	char i = '0';
+	int i = '0';
 
 	while
-		(i <= '9')
+		 (i <= '9')
 	{
 		putchar(i);
 		i++;
 	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
