4b1deb991e94d64ba769e0b3c2a15fa54c6916b6
smart wisdom <smartwisdom212@gmail.com>
2023-06-17 20:04:46 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints lowercase in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char d = 'z';
+
+	while
+		(d >= 'a')
+		{
+			putchar(d);
+			d--;
+		}
+	putchar('\n');
+	return (0);
+}
