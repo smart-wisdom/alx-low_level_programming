2d29e7b96d6bfc3010aa84dab5e06c3feacb33cd
smart wisdom <smartwisdom212@gmail.com>
2023-06-17 18:55:59 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - printing base ten digits without type char
+ * Return: 0
+ */
+int main(void)
+{
+	char i = '0';
+
+	while
+		(i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
