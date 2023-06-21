459ddd3b8f04f69e7b5072682866f18d0923af06
smart wisdom <smartwisdom212@gmail.com>
2023-06-20 21:21:47 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - function that adds two integers and returns the result
+ * @n1: check number the of first added integer
+ * @n2: check number of the second added integer
+ * Return: Value
+ */
+int add(int n2, int n1)
+{
+	return (n1 + n2);
+}
