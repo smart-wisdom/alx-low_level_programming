86be0b3e32716dde4537bd49fe2e793890425070
smart wisdom <smartwisdom212@gmail.com>
2023-06-20 18:12:30 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - function that checks for lowercase
+ * @c: Check the character
+ * Return: 1 for lowercase or 0 for anything else
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
