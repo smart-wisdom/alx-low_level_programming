ea8e3f60940ad01d7584f2a583c54fee4f06d141
smart wisdom <smartwisdom212@gmail.com>
2023-06-22 14:02:17 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - The program to stdout
+ * @c: character to print
+ * Return: 0
+ *//
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
