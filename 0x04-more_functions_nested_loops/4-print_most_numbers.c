54e3c9ae4c9398d0e841c17474f5e14c51317b72
smart wisdom <smartwisdom212@gmail.com>
2023-06-23 04:52:43 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - print 0 - 9 except 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	if (a == 2 || a == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(a + '0');
+	}
+	}
+	_putchar('\n');
+}
