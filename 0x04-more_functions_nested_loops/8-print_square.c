b60a8f7bcb4960bda1094c80a3f64cf486265205
smart wisdom <smartwisdom212@gmail.com>
2023-06-23 06:27:12 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - function that prints a square
+ * @size: The sizes of square
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (a = 0; a < size; a++)
+	{
+	for (b = 0; b < size; b++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
