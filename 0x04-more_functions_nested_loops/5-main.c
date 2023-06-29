0bb3cd3bf792fc75666884c41b69a7c1c696aaa2
smart wisdom <smartwisdom212@gmail.com>
2023-06-29 14:15:47 -0700

    README.md

--- 0x04-more_functions_nested_loops/5-main.c
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_alphabet();
+    more_numbers();
     return (0);
 }
--- 0x05-pointers_arrays_strings/7-main.c
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_alphabet_x10();
+    char *str;
+
+    str = "0123456789";
+    puts_half(str);
     return (0);
 }
