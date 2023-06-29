c4f15387ee13a7277190119220ab5f16e10c89b1
smart wisdom <smartwisdom212@gmail.com>
2023-06-29 14:47:46 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: Value to be added
+ * @src: Value to be added
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	y = 0;
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+
+	dest[x] = '\0';
+	return (dest);
+}
