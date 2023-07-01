c49cfcb736d5f157d57f5420c1aaa65c4698c460
smart wisdom <smartwisdom212@gmail.com>
2023-07-01 07:48:46 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,13 +7,11 @@
 
 int main(void)
 {
-	int n;
-	int a[5];
+	int a[5] = (65, 66, 67, 68, 69);
 	int *p;
 
 	a[2] = 1024;
-	p = &n;
-	*(p = &a[2]) = 98;
-	printf("a[2] = %d\n", a[2]);
+	p = a;
+	printf("a[2] = %d\n", *(p + 2));
 	return (0);
 }
