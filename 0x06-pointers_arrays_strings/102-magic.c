01038edda16679d0de8e3667fd0bba71fe2d7962
smart wisdom <smartwisdom212@gmail.com>
2023-07-01 10:05:57 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,15 +9,15 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * - Add one line to the code, so that the program prints a[2] = 98
+	 * - Write your one line of code here...
 	 * Remember
-	 * - You are not allowed to use the variable a in your new line of code
+	 * - You are not allowed to use the variable a
 	 * - You are not allowed to modify the variable p
 	 * - You can only write one statement
 	 * - You are not allowed to code anything else than the line of code at the expected line
 	 */
 	*(p + 5) = 98;
-	/* - The code should print 98\n */
+	/* - ...code should print 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
