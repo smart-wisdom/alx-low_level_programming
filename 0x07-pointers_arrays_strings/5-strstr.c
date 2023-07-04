4de3ea02cb0ea90d9c59dbab7a5d0d9809856f8f
smart wisdom <smartwisdom212@gmail.com>
2023-07-04 11:37:28 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - function that locates a substring
+ * @haystack: input
+ * @needle: input
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+		if (*b == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
