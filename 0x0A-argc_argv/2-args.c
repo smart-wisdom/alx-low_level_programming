440fad9d249590f3afc8e91503bd0780f0926daa
smart wisdom <smartwisdom212@gmail.com>
2023-07-12 16:42:47 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program that prints all arguments it receives
+ * @argc: argument number
+ * @argv: argument arrays
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
