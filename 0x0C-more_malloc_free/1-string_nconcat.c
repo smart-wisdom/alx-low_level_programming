2edfb6bcf0e03609ba0d552d9d546302e55ffe0d
smart wisdom <smartwisdom212@gmail.com>
2023-07-15 15:31:36 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,6 +34,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (n < length2 && i < (length1 + n))
 		s[i++] = s2[j++];
 
+	while (n >= length2 && i < (length1 + length2))
+		s[i++] = s2[j++];
+
 	s[i] = '\0';
 
 	return (s);
