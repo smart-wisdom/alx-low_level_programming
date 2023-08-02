0ee82814653aa9c453b73ec32ddc0de8d13c45da
smart wisdom <smartwisdom212@gmail.com>
2023-08-02 06:30:08 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,8 +13,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
-	if (!new_node || head)
+	if (!new_node || !head)
+	{
 		return (NULL);
+	}
 
 	new_node->n = n;
 	new_node->next = NULL;
