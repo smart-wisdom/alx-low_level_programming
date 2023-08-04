ecd2373f710012b485867cd455efb018cf7c4def
smart wisdom <smartwisdom212@gmail.com>
2023-08-04 10:19:01 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * clear_bit - function that sets the value of a bit to 0 at a given index
+ * @n: number of bit to clear
+ * @index: index of bit to clear
+ * Return: 1 if  worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = (~(1UL << index)) & *n;
+	return (1);
+}
